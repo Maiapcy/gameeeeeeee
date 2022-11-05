836e47dff4d587dcf1403195b691e2f4ab27d059
apichayaaaaa <65011183@kmitl.ac.th>
2022-11-05 17:27:36 +0700

    game

---
+++ levels/tilesets/palms.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="fg_palms" tilewidth="80" tileheight="136" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="80" height="103" source="../../graphics/terrain/plam_small/small_1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="78" height="136" source="../../graphics/terrain/palm_large/large_1.png"/>
+ </tile>
+</tileset>
